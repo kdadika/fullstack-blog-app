54c066efa733bd7ac5370753c5bbf408b0f140c4
Kyle <kdadika@gmail.com>
2023-04-11 23:29:33 -0400

    Completed frontend user login token authentication and user blog creation

--- blog-list-backend/controllers/blogs.js
+++ blog-list-backend/controllers/blogs.js
@@ -1,30 +1,35 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const middleware = require("../utils/middleware");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs.map((blog) => blog.toJSON()));
 });
 
-blogsRouter.post("/", async (request, response, next) => {
-  const { body } = request;
+blogsRouter.post(
+  "/",
+  middleware.userExtractor,
+  async (request, response, next) => {
+    const { body } = request;
 
-  const user = request.user;
+    const user = request.user;
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user._id,
-  });
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id,
+    });
 
-  const savedBlog = await blog.save();
-  user.blogs = user.blogs.concat(savedBlog._id);
-  await user.save();
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
 
-  response.status(201).json(savedBlog);
-});
+    response.status(201).json(savedBlog);
+  }
+);
 
 blogsRouter.get("/:id", async (request, response) => {
   try {
@@ -37,21 +42,25 @@ blogsRouter.get("/:id", async (request, response) => {
   }
 });
 
-blogsRouter.delete("/:id", async (request, response) => {
-  const { id } = request.params;
+blogsRouter.delete(
+  "/:id",
+  middleware.userExtractor,
+  async (request, response) => {
+    const { id } = request.params;
 
-  const user = request.user;
-  const blog = await Blog.findById(id);
+    const user = request.user;
+    const blog = await Blog.findById(id);
 
-  if (blog.user.toString() === user._id.toString()) {
-    await Blog.findByIdAndRemove(id);
-    response.status(204).end();
-  } else {
-    response
-      .status(401)
-      .json({ error: "You are not authorized to delete this blog" });
+    if (blog.user.toString() === user._id.toString()) {
+      await Blog.findByIdAndRemove(id);
+      response.status(204).end();
+    } else {
+      response
+        .status(401)
+        .json({ error: "You are not authorized to delete this blog" });
+    }
   }
-});
+);
 
 blogsRouter.put("/:id", async (request, response, next) => {
   const { body } = request;
--- blog-list-frontend/src/App.js
+++ blog-list-frontend/src/App.js
@@ -1,19 +1,45 @@
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
+import Notification from "./components/Notification";
+
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [newBlog, setNewBlog] = useState({ title: "", author: "", url: "" });
+  const [newBlog, setNewBlog] = useState({
+    title: "",
+    author: "",
+    url: "",
+    likes: 0,
+  });
   // const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [message, setMessage] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  // Clear notification after 5 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage(null);
+    }, 5000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message]);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
   }, []);
 
   const handleLogin = async (e) => {
@@ -24,16 +50,22 @@ const App = () => {
         username,
         password,
       });
+      window.localStorage.setItem("loggedBlogUser", JSON.stringify(user));
       blogService.setToken(user.token);
       setUser(user);
       setUsername("");
       setPassword("");
     } catch (exception) {
-      setErrorMessage("Wrong credentials");
+      setMessage("Wrong credentials");
       setTimeout(() => {
-        setErrorMessage(null);
+        setMessage(null);
       }, 5000);
     }
+  };
+
+  const handleLogout = () => {
+    window.localStorage.clear();
+    setUser(null);
   };
 
   const handleBlogChange = (e) => {
@@ -41,10 +73,25 @@ const App = () => {
     setNewBlog({ ...newBlog, [name]: value });
   };
 
-  const addBlog = (e) => {
+  const handleCreateBlog = (e) => {
     e.preventDefault();
-    addBlog(newBlog.title, newBlog.author, newBlog.url);
+    createBlog(newBlog.title, newBlog.author, newBlog.url, newBlog.likes);
     setNewBlog({ title: "", author: "", url: "" });
+  };
+
+  const createBlog = async (title, author, url, likes) => {
+    try {
+      const blog = await blogService.create({
+        title,
+        author,
+        url,
+        likes,
+      });
+      setBlogs(blogs.concat(blog));
+      setMessage(`A new blog ${title} by ${author} added`);
+    } catch (error) {
+      setMessage("error" + error.response.data.error);
+    }
   };
 
   const loginForm = () => (
@@ -72,7 +119,7 @@ const App = () => {
   );
 
   const blogForm = () => (
-    <form onSubmit={addBlog}>
+    <form onSubmit={handleCreateBlog}>
       <div>
         title
         <input
@@ -100,7 +147,7 @@ const App = () => {
           onChange={handleBlogChange}
         />
       </div>
-      <button id="create-blog-btn" type="submit">
+      <button id="createBtn" type="submit">
         create
       </button>
     </form>
@@ -108,41 +155,25 @@ const App = () => {
 
   return (
     <div>
-      <h2>blogs</h2>
-      <p>{errorMessage}</p>
+      <h1 className="headerTitle">blogs</h1>
+      <Notification message={message} />
       {!user && loginForm()}
       {user && (
         <div>
-          <p>{user.name} logged in</p>
+          <p>
+            <span className="active-user">{user.name}</span> logged in{" "}
+            <button id="logout-btn" onClick={handleLogout}>
+              logout
+            </button>
+          </p>
           {blogForm()}
+          <div>
+            {blogs.map((blog) => (
+              <Blog key={blog.id} blog={blog} />
+            ))}
+          </div>
         </div>
       )}
-      {/* <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form> */}
-      <div>
-        {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog} />
-        ))}
-      </div>
     </div>
   );
 };
